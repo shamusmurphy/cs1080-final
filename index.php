0f58ec71cff0b21fb28a902d1994a1d0db066ec6
shamusmurphy <143419523+shamusmurphy@users.noreply.github.com>
2023-12-01 13:15:46 -0500

    Final: created css and updated index

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<!DOCTYPE html>
+<?php
+include 'top.php';
+?>
+<main>
+<h2 class="box1">Introduction</h2>
 
-<p>Hey</p>
-
-<p> 1. Killington </p>
+<section class="box2">
+<p>Skiing in Vermont is a thrilling adventure, offering enthusiasts a winter wonderland of pristine slopes and breathtaking views. Vermont boasts a long list of world class ski resorts and skiers and snowboarders alike flock to these destinations to navigate the diverse terrain from steep glades to perfectly groomed trails. The state's small ski towns and cozy lodges add to the charm that makes skiing so special. With a rich history of winter sports and a commitment to keeping their facilities top notch, Vermont remains a top tier destination for those seeking to get away for the winter on the slopes.</p>
+</section>
+<section class="box3">
+    <figure>
+        <img src="images/vermont_skiing.png" alt="Vermont Mountains">
+        <figcaption>Beautiful Vermont mountains ( <cite><a href="https://www.vermontvacation.com/things-to-do/winter-recreation/ski-and-ride"> VermontVacation</a>)</figcaption>
+    </figure>
+</section>
+        </main>
