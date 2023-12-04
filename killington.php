5df2c805fadf65c2a6f6b244c246326fc01bbb84
Tyler Sheehan <143210854+TSheehan0127@users.noreply.github.com>
2023-12-04 13:15:43 -0500

    Add files via upload
    added more detail

--- killington.php
+++ killington.php
@@ -4,6 +4,6 @@ include 'top.php';
 <main>
     <h2 class="box1">Killington</h2>
     <section class="box2">
-        <p>This section is about Killington!</p>
+        <p>In the heart of Vermont lies Killington, a premier skiing destination. With 213 trails, it accommodates skiers of all levels, inviting beginners to explore its six peaks. Beyond skiing, Killington offers an array of activities like tubing and snowmobiling. This mountain escape provides an ideal retreat for those seeking the classic Northeast skiing experience. Its sprawling landscape caters to intermediate and advanced skiers while providing ample opportunities for newcomers. The diverse offerings, from thrilling adventures to serene slopes, make Killington an all-encompassing haven for winter enthusiasts. Whether you're craving excitement or relaxation, Killington delivers an unforgettable winter retreat in the Northeast.</p>
     </section>
 </main>
