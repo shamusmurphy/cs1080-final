6e7cfedff29148ad17f4da5b64853d1faf30955f
Miles Goldsmith <143364068+msgoldsm@users.noreply.github.com>
2023-12-04 13:58:58 -0500

    stowe.php: added description

--- stowe.php
+++ stowe.php
@@ -1,4 +1,7 @@
 <?php
 include 'top.php';
 ?>
+<p>
+  Nestled in the heart of Vermont’s Green Mountains, Stowe Mountain Resort is a premier destination for winter enthusiasts. Famous for its classic New England charm and its breathtaking scenery, Stowe offers a diverse array of skiing experiences across its 485 acres of skiable terrain. With an elevation of 3,625 feet and 116 trails spread across 12 lifts, Stowe offers terrain for skiers and snowboarders of any level. Stowe’s proximity to bigger cities such as Burlington, Montreal, Boston, and New York allows easy access for out-of-staters. Whether you're carving through fresh powder on the slopes or exploring the vibrant surroundings, Stowe's timeless glamor makes it a must-visit destination for those seeking an authentic Vermont experience.
+</p>
 <main>
