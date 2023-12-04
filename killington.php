2a946d8af3b5f1a734e34afdec84270de2cd2cfb
Tyler Sheehan <143210854+TSheehan0127@users.noreply.github.com>
2023-12-04 13:43:07 -0500

    Add files via upload
    added image for Killington

--- killington.php
+++ killington.php
@@ -6,4 +6,10 @@ include 'top.php';
     <section class="box2">
         <p>In the heart of Vermont lies Killington, a premier skiing destination. With 213 trails, it accommodates skiers of all levels, inviting beginners to explore its six peaks. Beyond skiing, Killington offers an array of activities like tubing and snowmobiling. This mountain escape provides an ideal retreat for those seeking the classic Northeast skiing experience. Its sprawling landscape caters to intermediate and advanced skiers while providing ample opportunities for newcomers. The diverse offerings, from thrilling adventures to serene slopes, make Killington an all-encompassing haven for winter enthusiasts. Whether you're craving excitement or relaxation, Killington delivers an unforgettable winter retreat in the Northeast.</p>
     </section>
+    <section class ="box3"> 
+        <figure>
+            <img src="images/KillingtonMountain.jpeg" alt="Killington Mountain">
+            <figcaption> Image of Killington <br> ( <cite><a href="https://www.killingtongroup.com/the-history-of-killington-ski-resort-and-our-surrounding-town/">Killington vacation rentals </a>)</figcaption>
+        </figure>
+    </section>
 </main>
