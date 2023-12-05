bcd9727c7f9e9fbfc1bacf6e0b77eec3b11338ce
shamusmurphy <143419523+shamusmurphy@users.noreply.github.com>
2023-12-04 19:23:03 -0500

    Final: Updated some stuff

--- index.php
+++ index.php
@@ -10,7 +10,21 @@ include 'top.php';
 <section class="box3">
     <figure>
         <img src="images/vermont_skiing.png" alt="Vermont Mountains">
-        <figcaption>Beautiful Vermont mountains ( <cite><a href="https://www.vermontvacation.com/things-to-do/winter-recreation/ski-and-ride"> VermontVacation</a>)</figcaption>
+        <figcaption>Beautiful Vermont mountains (<cite><a href="https://www.vermontvacation.com/things-to-do/winter-recreation/ski-and-ride"> VermontVacation</a>)</figcaption>
+    </figure>
+</section>
+<section class="box4">
+    <h2>Mountains</h2>
+    <p>Killington, Stowe, and Sugarbush are three iconic ski resorts nestled in the picturesque landscapes of Vermont, USA, offering enthusiasts a thrilling winter wonderland experience. Killington, often referred to as the "Beast of the East," boasts the largest ski area in the eastern United States. With diverse terrain and a reputation for reliable snowfall, Killington provides skiers and snowboarders with a variety of challenges, from gentle slopes for beginners to steep, challenging trails for advanced riders. The resort's extensive trail network, modern amenities, and vibrant après-ski scene make it a sought-after destination for winter sports enthusiasts.</p>
+
+    <p>Stowe, nestled in the heart of the Green Mountains, is renowned for its classic New England charm and challenging slopes. With its iconic peak, Mount Mansfield, Stowe offers a mix of terrain suitable for all skill levels. Skiers can navigate through tree-lined trails, experience the thrill of well-groomed slopes, and enjoy breathtaking views of the surrounding mountains. Stowe's quaint village, complete with charming shops and restaurants, enhances the overall experience, making it a destination that combines outdoor adventure with a cozy atmosphere.</p>
+    
+    <p>Sugarbush, situated in the Mad River Valley, is known for its diverse terrain and welcoming atmosphere. The resort features two interconnected mountains, Lincoln Peak and Mt. Ellen, offering a range of skiing and snowboarding experiences. Sugarbush is especially beloved for its laid-back vibe, varied trails, and the legendary "Sugarbush Horseshoe," a challenging route that tests the skills of advanced skiers. With its commitment to preserving the natural beauty of the area, Sugarbush provides a unique and memorable winter escape for both seasoned athletes and those new to the world of snow sports.</p>
+</section>
+<section class="box5">
+    <figure>
+        <img src="images/stowe1.jpeg" alt="Stowe, Vermont">
+        <figcaption>Stowe, Vermont (<cite><a href="https://www.skimaven.com/post/getting-some-long-runs-in-at-vermonts-stowe-mountain-resort"> Ski Maven</a>)</figcaption>
     </figure>
 </section>
         </main>
