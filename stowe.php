02badab5aee5c0b00f017b3ab745cc8fea42cf76
Miles Goldsmith <143364068+msgoldsm@users.noreply.github.com>
2023-12-06 13:29:34 -0500

    stowe.php: added image and link

--- stowe.php
+++ stowe.php
@@ -2,9 +2,16 @@
 include 'top.php';
 ?>
 <main>
-<p>
-  Nestled in the heart of Vermont’s Green Mountains, Stowe Mountain Resort is a premier destination for winter enthusiasts. Famous for its classic New England charm and its breathtaking scenery, Stowe offers a diverse array of skiing experiences across its 485 acres of skiable terrain. With an elevation of 3,625 feet and 116 trails spread across 12 lifts, Stowe offers terrain for skiers and snowboarders of any level. Stowe’s proximity to bigger cities such as Burlington, Montreal, Boston, and New York allows easy access for out-of-staters. Whether you're carving through fresh powder on the slopes or exploring the vibrant surroundings, Stowe's timeless glamor makes it a must-visit destination for those seeking an authentic Vermont experience.
-</p>
+    <h2 class="box1">Stowe</h2>
+    <section class="box2">
+        <p>Nestled in the heart of Vermont’s Green Mountains, Stowe Mountain Resort is a premier destination for winter enthusiasts. Famous for its classic New England charm and its breathtaking scenery, Stowe offers a diverse array of skiing experiences across its 485 acres of skiable terrain. With an elevation of 3,625 feet and 116 trails spread across 12 lifts, Stowe offers terrain for skiers and snowboarders of any level. Stowe’s proximity to bigger cities such as Burlington, Montreal, Boston, and New York allows easy access for out-of-staters. Whether you're carving through fresh powder on the slopes or exploring the vibrant surroundings, Stowe's timeless glamor makes it a must-visit destination for those seeking an authentic Vermont experience.</p>
+    </section>
+    <section class ="box3"> 
+        <figure>
+            <img src="images/stoweview.jpg" alt="Stowe Mountain">
+            <figcaption> Image of Stowe <br> ( <cite><a href="https://gostowe.com/lodging/vacation-packages/">Stowe vacation packages</a>)</figcaption>
+        </figure>
+    </section>
 </main>
 <?php
 include "footer.php";
