9be7d42109bb7adffd356b9e420ce6cbc878d49d
Miles Goldsmith <143364068+msgoldsm@users.noreply.github.com>
2023-12-06 13:56:21 -0500

    sugarbush.php: added table

--- sugarbush.php
+++ sugarbush.php
@@ -2,9 +2,39 @@
 include 'top.php';
 ?>
 <main>
-<p>
-  Without trying to seem like a ritzy resort, Sugarbush does what it does best, which is pure, authentic, east coast skiing. Featuring six peaks and two mountains, as well as 111 trails spread across 484 acres, Sugarbush is one of the largest ski resorts in Vermont, not to mention its 2000+ acres of backcountry skiing available. Sugarbush has some of the most gnarly terrain on the east coast, with a good combination of some wide green trails and thrilling woody runs, all spread over a vertical drop of 2,600 feet. Sugarbush is also one of the busiest resorts in Vermont, as it’s close to Burlington, Vermont’s biggest city, and it’s also not a horrible drive for tourists from elsewhere in New England and New York.
-</p>
+    <h2 class="box1">Sugarbush</h2>
+    <section class="box2">
+        <p>Without trying to seem like a ritzy resort, Sugarbush does what it does best, which is pure, authentic, east coast skiing. Featuring six peaks and two mountains, as well as 111 trails spread across 484 acres, Sugarbush is one of the largest ski resorts in Vermont, not to mention its 2000+ acres of backcountry skiing available. Sugarbush has some of the most gnarly terrain on the east coast, with a good combination of some wide green trails and thrilling woody runs, all spread over a vertical drop of 2,600 feet. Sugarbush is also one of the busiest resorts in Vermont, as it’s close to Burlington, Vermont’s biggest city, and it’s also not a horrible drive for tourists from elsewhere in New England and New York.</p>
+    </section>
+    <section class ="box3"> 
+        <figure>
+            <img src="images/sugarbushview.jpg" alt="Sugarbush Mountain">
+            <figcaption> Image of Sugarbush <br> ( <cite><a href="https://www.sugarbush.com/plan-your-trip/lodging/resort-condos">Sugarbush vacation packages </a>)</figcaption>
+        </figure>
+    </section>
+    <section>
+        <table>
+            <tr>
+                <th>Year</th>
+                <th>Snowfall</th>
+            </tr>
+            <tr>
+                <td>2023</td>
+                <td>50"</td>
+            </tr>
+            <tr>
+                <td>2022</td>
+                <td>126"</td>
+            </tr>
+            <tr>
+                <td>2021</td>
+                <td>188"</td>
+            </tr>
+            <tr>
+                <td>2020</td>
+                <td>133"</td>
+            </tr>
+        </table>
 </main>
 <?php
 include "footer.php";
