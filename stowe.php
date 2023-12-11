95023deb73b4e63eea5b8f09a1cb001dc9ed5fef
Miles Goldsmith <143364068+msgoldsm@users.noreply.github.com>
2023-12-10 21:23:46 -0500

    stowe.php: added more images/words

--- stowe.php
+++ stowe.php
@@ -2,15 +2,21 @@
 include 'top.php';
 ?>
 <main>
-    <h2 class="box1">Stowe</h2>
-    <section class="box2">
-        <p>Nestled in the heart of Vermont’s Green Mountains, Stowe Mountain Resort is a premier destination for winter enthusiasts. Famous for its classic New England charm and its breathtaking scenery, Stowe offers a diverse array of skiing experiences across its 485 acres of skiable terrain. With an elevation of 3,625 feet and 116 trails spread across 12 lifts, Stowe offers terrain for skiers and snowboarders of any level. Stowe’s proximity to bigger cities such as Burlington, Montreal, Boston, and New York allows easy access for out-of-staters. Whether you're carving through fresh powder on the slopes or exploring the vibrant surroundings, Stowe's timeless glamor makes it a must-visit destination for those seeking an authentic Vermont experience.</p>
-    </section>
+    <h2 class="box1">Stowe</h2>   
     <section class ="box3"> 
         <figure>
             <img src="images/stoweview.jpg" alt="Stowe Mountain">
             <figcaption> Image of Stowe <br> ( <cite><a href="https://gostowe.com/lodging/vacation-packages/">Stowe vacation packages</a>)</figcaption>
         </figure>
+    </section>
+    <section class="box2">
+        <p>Nestled in the heart of Vermont’s Green Mountains, Stowe Mountain Resort is a premier destination for winter enthusiasts. Famous for its classic New England charm and its breathtaking scenery, Stowe offers a diverse array of skiing experiences across its 485 acres of skiable terrain. With an elevation of 3,625 feet and 116 trails spread across 12 lifts, Stowe offers terrain for skiers and snowboarders of any level. Stowe’s proximity to bigger cities such as Burlington, Montreal, Boston, and New York allows easy access for out-of-staters. Whether you're carving through fresh powder on the slopes or exploring the vibrant surroundings, Stowe's timeless glamor makes it a must-visit destination for those seeking an authentic Vermont experience.</p>
+    </section>
+    <section class="">
+        <img src="images/stowepic.jpg" alt="Stowe Skiers">
+    </section>
+    <section class="">
+        <p>A visit to Stowe extends beyond its winter allure, revealing a year-round haven for outdoor enthusiasts. As the snow retreats, the mountain landscape transforms into a paradise for hikers, bikers, and nature enthusiasts. Stowe's extensive trail network invites adventurers to explore dense forests, meandering streams, and panoramic vistas. The quaint village, with its boutique shops and farm-to-table dining, beckons visitors to indulge in the charm of rural Vermont life. Throughout the warmer months, Stowe seamlessly marries outdoor recreation with cultural experiences, ensuring that its appeal endures beyond the snowy season.</p>
     </section>
 </main>
 <?php
