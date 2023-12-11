cee5b034c279f497c22c6bddefd6e875c6ea9d81
Miles Goldsmith <143364068+msgoldsm@users.noreply.github.com>
2023-12-10 23:02:16 -0500

    sugarbush.php: added images/words

--- sugarbush.php
+++ sugarbush.php
@@ -3,37 +3,61 @@ include 'top.php';
 ?>
 <main>
     <h2 class="box1">Sugarbush</h2>
-    <section class="box2">
-        <p>Without trying to seem like a ritzy resort, Sugarbush does what it does best, which is pure, authentic, east coast skiing. Featuring six peaks and two mountains, as well as 111 trails spread across 484 acres, Sugarbush is one of the largest ski resorts in Vermont, not to mention its 2000+ acres of backcountry skiing available. Sugarbush has some of the most gnarly terrain on the east coast, with a good combination of some wide green trails and thrilling woody runs, all spread over a vertical drop of 2,600 feet. Sugarbush is also one of the busiest resorts in Vermont, as it’s close to Burlington, Vermont’s biggest city, and it’s also not a horrible drive for tourists from elsewhere in New England and New York.</p>
-    </section>
     <section class ="box3"> 
         <figure>
             <img src="images/sugarbushview.jpg" alt="Sugarbush Mountain">
-            <figcaption> Sugarbush on a clear day. (<cite><a href="https://www.sugarbush.com/plan-your-trip/lodging/resort-condos">Sugarbush vacation packages</a>)</figcaption>
+            <figcaption> Image of Sugarbush <br> ( <cite><a href="https://www.sugarbush.com/plan-your-trip/lodging/resort-condos">Sugarbush vacation packages </a>)</figcaption>
         </figure>
     </section>
-    <section class="box4">
+    <section class="box2">
+        <p>Without trying to seem like a ritzy resort, Sugarbush does what it does best, which is pure, authentic, east coast skiing. Featuring six peaks and two mountains, as well as 111 trails spread across 484 acres, Sugarbush is one of the largest ski resorts in Vermont, not to mention its 2000+ acres of backcountry skiing available. Sugarbush has some of the most gnarly terrain on the east coast, with a good combination of some wide green trails and thrilling woody runs, all spread over a vertical drop of 2,600 feet. Sugarbush is also one of the busiest resorts in Vermont, as it’s close to Burlington, Vermont’s biggest city, and it’s also not a horrible drive for tourists from elsewhere in New England and New York.</p>
+    </section>    
+    <section class ="box5">
         <table>
-            <caption>Yearly Snowfall at Sugarbush</caption>
             <tr>
                 <th>Year</th>
-                <th>Snowfall (inches)</th>
+                <th>Snowfall</th>
             </tr>
-            <?php
-            $sql = 'SELECT fldyear, fldsnow FROM tblSnowfall';
-            $statement = $pdo->prepare($sql);
-            $statement->execute();
-
-            $records = $statement->fetchAll();
-
-            foreach($records as $record){
-                print '<tr>';
-                print '<td>' .$record['fldyear'] . '</td>';
-                print '<td>' .$record['fldsnow'] . '"</td>';
-                print '</tr>' . PHP_EOL;
-            }
-            ?>
+            <tr>
+                <td>2023</td>
+                <td>50"</td>
+            </tr>
+            <tr>
+                <td>2022</td>
+                <td>126"</td>
+            </tr>
+            <tr>
+                <td>2021</td>
+                <td>188"</td>
+            </tr>
+            <tr>
+                <td>2020</td>
+                <td>133"</td>
+            </tr>
+            <tr>
+                <td>2019</td>
+                <td>102"</td>
+            </tr>
+            <tr>
+                <td>2018</td>
+                <td>173"</td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>186"</td>
+            </tr>
+            <tr>
+                <td>2016</td>
+                <td>412"</td>
+            </tr>
         </table>
+    </section>
+    <section class="">
+        <img src="images/sugarbushlodge.jpg" alt="Sugarbush Lodge">
+    </section>
+    <section class="">
+        <p>Nestled in the heart of the Mad River Valley, Sugarbush stands as a haven for outdoor enthusiasts year-round. Beyond its winter prowess, the extensive trail network transforms into a haven for hikers and bikers, offering an immersive experience amid lush forests and panoramic vistas. The quaint village, with its local shops and welcoming cafes, provides a charming backdrop for those seeking a genuine taste of Vermont's relaxed atmosphere. Whether carving through pristine powder or embarking on a summer adventure, Sugarbush's authentic appeal stands as a testament to the diverse beauty of East Coast skiing.</p>
+    </section>
 </main>
 <?php
 include "footer.php";
